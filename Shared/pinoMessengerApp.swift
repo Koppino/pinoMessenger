019edbbacee99ac3a59e8c6a79e06f5a20b0c007
Radek Kopp <fivezeros@seznam.cz>
2020-09-09 23:05:37 +0200

    Initial Commit

---
+++ Shared/pinoMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinoMessengerApp.swift
+//  Shared
+//
+//  Created by Radek Kopp on 09/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct pinoMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
